72c8cb2dae565c9f4644eebf602c0c8d4c2a23dd
Lemuriets <madyarovegor@gmail.com>
2026-02-08 16:57:31 +0500

    added tests for CanParser

--- LogDecoder.Can.Tests/AllTest.cs
+++ LogDecoder.Can.Tests/AllTest.cs
@@ -1,15 +1,116 @@
+using LogDecoder.CAN;
+
 namespace LogDecoder.Can.Tests;
 
-public class Tests
+[TestFixture]
+public class CanPackageParserTests
 {
-    [SetUp]
-    public void Setup()
+    [Test]
+    public void FromBytes_ShouldParseStandardPackage()
     {
+        // Arrange
+        byte dataLength = 3;
+        byte firstByte = dataLength; // < 0x80 => Standard
+        int id = 0x1234;
+        byte[] expectedData = { 0xAA, 0xBB, 0xCC };
+        int hrc = 0x010203;
+
+        var raw = new byte[]
+        {
+            firstByte,
+            0x34, 0x12,                 // ID (2 bytes little endian)
+            0xAA, 0xBB, 0xCC,           // Data
+            0x03, 0x02, 0x01            // HRC (3 bytes little endian)
+        };
+
+        // Act
+        var package = CanPackageParser.FromBytes(raw);
+
+        // Assert
+        Assert.That(package.Type, Is.EqualTo(PackageType.Standard));
+        Assert.That(package.Id, Is.EqualTo(id));
+        Assert.That(package.Data, Is.EqualTo(expectedData));
+        Assert.That(package.Hrc, Is.EqualTo(hrc));
+        Assert.That(package.Length, Is.EqualTo(1 + 2 + 3 + 3));
     }
 
     [Test]
-    public void Test1()
+    public void FromBytes_ShouldParseExtendedPackage()
     {
-        Assert.Pass();
+        // Arrange
+        byte dataLength = 2;
+        byte firstByte = (byte)(0x80 | dataLength); // >= 0x80 => Extended
+        int id = 0x11223344;
+        byte[] expectedData = { 0x10, 0x20 };
+        int hrc = 0x000102;
+
+        var raw = new byte[]
+        {
+            firstByte,
+            0x44, 0x33, 0x22, 0x11,     // ID (4 bytes little endian)
+            0x10, 0x20,                 // Data
+            0x02, 0x01, 0x00            // HRC (3 bytes little endian)
+        };
+
+        // Act
+        var package = CanPackageParser.FromBytes(raw);
+
+        // Assert
+        Assert.That(package.Type, Is.EqualTo(PackageType.Extended));
+        Assert.That(package.Id, Is.EqualTo(id));
+        Assert.That(package.Data, Is.EqualTo(expectedData));
+        Assert.That(package.Hrc, Is.EqualTo(hrc));
+        Assert.That(package.Length, Is.EqualTo(1 + 4 + 2 + 3));
+    }
+
+    [Test]
+    public void FromBytes_ShouldReturnEmptyPackage_WhenDataSizeTooLarge()
+    {
+        // Arrange
+        byte firstByte = 0x09; // data size = 9 (> MaxDataSize = 8)
+
+        var raw = new byte[]
+        {
+            firstByte,
+            0x00, 0x00,
+            0x00, 0x00, 0x00
+        };
+
+        // Act
+        var package = CanPackageParser.FromBytes(raw);
+
+        // Assert
+        Assert.That(package, Is.EqualTo(default(CanPackage)));
+    }
+
+    [TestCase(0x01, PackageType.Standard)]
+    [TestCase(0x7F, PackageType.Standard)]
+    [TestCase(0x80, PackageType.Extended)]
+    [TestCase(0xFF, PackageType.Extended)]
+    public void GetPackageType_ShouldReturnCorrectType(byte firstByte, PackageType expected)
+    {
+        var type = CanPackageParser.GetPackageType(firstByte);
+
+        Assert.That(type, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void GetPackageLength_ShouldCalculateCorrectLength_Standard()
+    {
+        byte firstByte = 0x03; // data size = 3
+
+        var length = CanPackageParser.GetPackageLength(firstByte, PackageType.Standard);
+
+        Assert.That(length, Is.EqualTo(1 + 2 + 3 + 3));
+    }
+
+    [Test]
+    public void GetPackageLength_ShouldCalculateCorrectLength_Extended()
+    {
+        byte firstByte = 0x82; // data size = 2 (0x82 & 0x7F)
+
+        var length = CanPackageParser.GetPackageLength(firstByte, PackageType.Extended);
+
+        Assert.That(length, Is.EqualTo(1 + 4 + 2 + 3));
     }
 }
--- LogDecoder.Can/CanPackageParser.cs
+++ LogDecoder.Can/CanPackageParser.cs
@@ -31,6 +31,10 @@ public static class CanPackageParser
         {
             return new CanPackage();
         }
+        if (length > rawPackage.Length)
+        {
+            return new CanPackage();
+        }
         var hrc = GetHrc(rawPackage, type, data.Length);
 
         return new CanPackage(type, id, data, hrc, length);
@@ -85,121 +89,3 @@ public static class CanPackageParser
          }
     }
 }
-
-// ОПТИМИЗАЦИЯ ОТ ГПТ
-
-// using System.Runtime.CompilerServices;
-//
-// namespace LogDecoder.CAN;
-//
-// public static class CanPackageParser
-// {
-//     private const int LengthSize = 1;
-//     private const int HrcSize = 3;
-//     private const int MaxDataSize = 8;
-//
-//     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//     public static CanPackage? FromBytes(byte[] rawPackage)
-//     {
-//         if (rawPackage.Length == 0 || rawPackage.Length < LengthSize + 1)
-//             return null;
-//
-//         var first = rawPackage[0];
-//         var type = GetPackageType(first);
-//         var dataSize = GetDataSize(first);
-//         if (dataSize == 0 || dataSize > MaxDataSize)
-//             return null;
-//
-//         var idLen = (int)type;
-//         var length = LengthSize + idLen + dataSize + HrcSize;
-//         if (rawPackage.Length < length)
-//             return null;
-//
-//         var id = ReadInt32Little(rawPackage, LengthSize, idLen);
-//
-//         var data = new byte[dataSize];
-//         if (dataSize > 0)
-//             Buffer.BlockCopy(rawPackage, LengthSize + idLen, data, 0, dataSize);
-//
-//         var hrc = ReadInt32Little(rawPackage, LengthSize + idLen + dataSize, HrcSize);
-//
-//         return new CanPackage(type, id, data, hrc, length);
-//     }
-//
-//     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//     public static CanPackage? FromBytes(byte[] rawPackage, PackageType type, int length, int id)
-//     {
-//         if (rawPackage.Length == 0)
-//             return null;
-//
-//         if (rawPackage.Length < LengthSize + (int)type)
-//             return null;
-//
-//         var dataSize = GetDataSize(rawPackage[0]);
-//         if (dataSize == 0 || dataSize > MaxDataSize)
-//             return null;
-//
-//         if (rawPackage.Length < length)
-//             return null;
-//
-//         var data = new byte[dataSize];
-//         if (dataSize > 0)
-//             Buffer.BlockCopy(rawPackage, LengthSize + (int)type, data, 0, dataSize);
-//
-//         var hrc = ReadInt32Little(rawPackage, LengthSize + (int)type + dataSize, HrcSize);
-//
-//         return new CanPackage(type, id, data, hrc, length);
-//     }
-//
-//     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//     public static int GetPackageLength(byte firstByte, PackageType type)
-//     {
-//         return LengthSize + (int)type + GetDataSize(firstByte) + HrcSize;
-//     }
-//
-//     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//     private static int GetDataSize(byte firstByte) => firstByte & 0x7F;
-//
-//     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//     public static PackageType GetPackageType(byte firstByte) => firstByte < 0x80 ? PackageType.Standard : PackageType.Extended;
-//
-//     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//     public static int GetPackageId(byte[] raw, PackageType type)
-//     {
-//         return ReadInt32Little(raw, LengthSize, (int)type);
-//     }
-//
-//     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//     private static byte[] GetData(byte[] raw, PackageType type)
-//     {
-//         var size = GetDataSize(raw[0]);
-//         if (size == 0 || size > MaxDataSize)
-//             return Array.Empty<byte>();
-//
-//         var start = LengthSize + (int)type;
-//         var data = new byte[size];
-//         Buffer.BlockCopy(raw, start, data, 0, size);
-//         return data;
-//     }
-//
-//     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//     private static int GetHrc(byte[] raw, PackageType type, int dataSize)
-//     {
-//         var start = LengthSize + (int)type + dataSize;
-//         return ReadInt32Little(raw, start, HrcSize);
-//     }
-//
-//     // Читает count байт (1..4) в little-endian порядок и возвращает int.
-//     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//     private static int ReadInt32Little(byte[] raw, int offset, int count)
-//     {
-//         unchecked
-//         {
-//             var b0 = count > 0 ? raw[offset] : 0;
-//             var b1 = count > 1 ? raw[offset + 1] : 0;
-//             var b2 = count > 2 ? raw[offset + 2] : 0;
-//             var b3 = count > 3 ? raw[offset + 3] : 0;
-//             return b0 | (b1 << 8) | (b2 << 16) | (b3 << 24);
-//         }
-//     }
-// }
