e02102f50098cf577b151075eda7b63cf0ddee09
Lemuriets <madyarovegor@gmail.com>
2026-04-02 09:14:21 +0500

    changed packages names

--- LogDecoder.Can/CanPackages/Factory.cs
+++ LogDecoder.Can/CanPackages/Factory.cs
@@ -47,33 +47,33 @@ public class CanPackageFactory : ICanPackageFactory
 
     private void RegisterBuiltIn()
     {
-        Register(0x4A0, "Режим КИВЛ", (p, n, c) => new IdModeCivl(p, n));
-        Register(0x4A9, "Ошибки калибровки КИВЛ", (p, n, c) => new IdClbrErrCivl(p, n, c.CivlMode));
-        Register(0x401, "Состояние источника питания", (p, n, c) => new IdStatusPwr(p, n));
-        Register(0x4C3, "Состояние ПП", (p, n, c) => new IdStatusCapno2(p, n));
-        Register(0x442, "Состояние платы смесителя газов", (p, n, c) => new IdStatusMix(p, n));
-        Register(0x422, "Состояние платы CAN-Ethernet", (p, n, c) => new IdStatusScm(p, n));
-        Register(0x4AD, "Измеренный объём выдоха", (p, n, c) => new IdMVexpCivl(p, n));
-        Register(0x4AE, "PEEP, AutoPEEP и поток на вдохе", (p, n, c) => new IdMPeepCivl(p, n));
-        Register(0x4A8, "Состояние КИВЛ", (p, n, c) => new IdStatusCivl(p, n));
-        Register(0x510, "Состояние КИВЛ 2", (p, n, c) => new IdStatusCivl2(p, n));
-        Register(0x4AF, "Частота дыхания", (p, n, c) => new IdMRbCivl(p, n));
-        Register(0x5A9, "Состояние контроллера мотора", (p, n, c) => new IdStatusMotor(p, n));
-        Register(0x4B0, "Пиковое давление и стресс-индекс", (p, n, c) => new IdMPipCivl(p, n));
-        Register(0x581, "Состояние Masimo 1", (p, n, c) => new IdStatusSpo_v2_1(p, n));
-        Register(0x582, "Состояние Masimo 2", (p, n, c) => new IdStatusSpo_v2_2(p, n));
-        Register(0x4AC, "Измеренный объём вдоха", (p, n, c) => new IdMVinspCivl(p, n));
-        Register(0x4B2, "Время выдоха", (p, n, c) => new IdMTexpCivl(p, n));
-        Register(0x4A6, "Графическая информация", (p, n, c) => new IdWaveCivl(p, n));
-        Register(1120, "Синхро-пакет", (p, n, c) => new IdSynchro(p, n));
-        Register(0x482, "Состояние метаболографа", (p, n, c) => new IdStatusCapno1(p, n));
-        Register(0x4E1, "Состояния Treaton", (p, n, c) => new IdStatusSpo(p, n));
-        Register(0x4B5, "Информация об утечке", (p, n, c) => new IdMLeakCivl(p, n));
-        Register(0x4B3, "Время вдоха и RCinsp", (p, n, c) => new IdMTinspCivl(p, n));
-        Register(0x445, "Потоки воздух/кислород", (p, n, c) => new IdFlowMix(p, n));
-        Register(0x4A3, "Параметры вентиляции 1", (p, n, c) => new IdPar1Civl(p, n));
-        Register(0x4A4, "Параметры вентиляции 2", (p, n, c) => new IdPar2Civl(p, n));
-        Register(0x4A5, "Параметры вентиляции 3", (p, n, c) => new IdPar3Civl(p, n));
-        Register(0x50E, "Параметры вентиляции 5", (p, n, c) => new IdPar5Civl2(p, n));
+        Register(0x4A0, "ID_MODE_CIVL", (p, n, c) => new IdModeCivl(p, n));
+        Register(0x4A9, "ID_CLBR_ERR_CIVL", (p, n, c) => new IdClbrErrCivl(p, n, c.CivlMode));
+        Register(0x401, "ID_STATUS_PWR", (p, n, c) => new IdStatusPwr(p, n));
+        Register(0x4C3, "ID_STATUS_CAPNO2", (p, n, c) => new IdStatusCapno2(p, n));
+        Register(0x442, "ID_STATUS_MIX", (p, n, c) => new IdStatusMix(p, n));
+        Register(0x422, "ID_STATUS_SCM", (p, n, c) => new IdStatusScm(p, n));
+        Register(0x4AD, "ID_M_VEXP_CIVL", (p, n, c) => new IdMVexpCivl(p, n));
+        Register(0x4AE, "ID_M_PEEP_CIVL", (p, n, c) => new IdMPeepCivl(p, n));
+        Register(0x4A8, "ID_STATUS_CIVL", (p, n, c) => new IdStatusCivl(p, n));
+        Register(0x510, "ID_STATUS2_CIVL2", (p, n, c) => new IdStatusCivl2(p, n));
+        Register(0x4AF, "ID_M_RB_CIVL", (p, n, c) => new IdMRbCivl(p, n));
+        Register(0x5A9, "ID_STATUS_MOTOR", (p, n, c) => new IdStatusMotor(p, n));
+        Register(0x4B0, "ID_M_PIP_CIVL", (p, n, c) => new IdMPipCivl(p, n));
+        Register(0x581, "ID_STATUS_SPO_V2_1", (p, n, c) => new IdStatusSpo_v2_1(p, n));
+        Register(0x582, "ID_STATUS_SPO_V2_2", (p, n, c) => new IdStatusSpo_v2_2(p, n));
+        Register(0x4AC, "ID_M_VINSP_CIVL", (p, n, c) => new IdMVinspCivl(p, n));
+        Register(0x4B2, "ID_M_TEXP_CIVL", (p, n, c) => new IdMTexpCivl(p, n));
+        Register(0x4A6, "ID_WAVE_CIVL", (p, n, c) => new IdWaveCivl(p, n));
+        Register(0x460, "ID_SYNHRO", (p, n, c) => new IdSynchro(p, n));
+        Register(0x482, "ID_STATUS_CAPNO1", (p, n, c) => new IdStatusCapno1(p, n));
+        Register(0x4E1, "ID_STATUS_SPO", (p, n, c) => new IdStatusSpo(p, n));
+        Register(0x4B5, "ID_M_LEAK_CIVL", (p, n, c) => new IdMLeakCivl(p, n));
+        Register(0x4B3, "ID_M_TINSP_CIVL", (p, n, c) => new IdMTinspCivl(p, n));
+        Register(0x445, "ID_FLOW_MIX", (p, n, c) => new IdFlowMix(p, n));
+        Register(0x4A3, "ID_PAR1_CIVL", (p, n, c) => new IdPar1Civl(p, n));
+        Register(0x4A4, "ID_PAR2_CIVL", (p, n, c) => new IdPar2Civl(p, n));
+        Register(0x4A5, "ID_PAR3_CIVL", (p, n, c) => new IdPar3Civl(p, n));
+        Register(0x50E, "ID_PAR5_CIVL2", (p, n, c) => new IdPar5Civl2(p, n));
     }
 }
